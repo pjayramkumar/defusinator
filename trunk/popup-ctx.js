d79ac32a26f826a5d21b116e244f4c728869fa01
tamas.rudnai@gmail.com <tamas.rudnai@gmail.com@256e7cb4-b457-f88c-69e9-8ec8e58dcd20>
2012-03-27 14:48:15 +0000

    Improved context menu - ctxMenu is now based on mouse events - no a/href, but window.open based handler - whois changed to domaintools

--- trunk/popup-ctx.js
+++ trunk/popup-ctx.js
@@ -3,30 +3,67 @@
 * by Tamas Rudnai, all rights reserved
 *************************************************************/
 
-document.onclick=check; 
-var popUpNew = 0;
-function check(e){ 
-    var target = (e && e.target) || (event && event.srcElement); 
-    var ctxMenu = document.getElementById('ctxMenu'); 
-    if ( ( target != ctxMenu ) && ( 0 >= popUpNew-- ) ) {
+var ctxX = 0;
+var ctxY = 0;
+var ctxMargin = 50;
+
+function hideCtxMenu(ctxMenu) {
+    if ( 0 >= popUpNew-- ) {
         popUpNew = 0; // make sure we are not going negative here!
         ctxMenu.style.display='none';
     } 
+}
+
+function onmouseleave(e) { 
+    var x = e.pageX;
+    var y = e.pageY;
+    var ctxMenu = document.getElementById('ctxMenu'); 
+
+    if ( ctxMenu ) {
+        // check if we are far away from the context menu
+        if ( ( x < ctxX - ctxMargin )
+          || ( x > ctxX + ctxMargin + ctxMenu.offsetWidth )
+          || ( y < ctxY - ctxMargin )
+          || ( y > ctxY + ctxMargin + ctxMenu.offsetHeight ) )
+        {
+            hideCtxMenu(ctxMenu);
+        }
+    }
 } 
 
-function clickCtx(menu) {
-    document.body.innerHTML = "CTX: " + menu.elements['a'];
+var popUpNew = 0;
+
+function check(e) { 
+    var target = (e && e.target) || (event && event.srcElement); 
+    var ctxMenu = document.getElementById('ctxMenu'); 
+    if ( ( ctxMenu ) && ( target != ctxMenu ) ) {
+        hideCtxMenu(ctxMenu);
+    } 
+} 
+
+// Opening URLs in a separated tab or window
+function openURL(url) {
+//    alert(url);
+    window.open(url,'_blank');
 }
 
 // we need the request in a global area otherwise will loose it
 var ctxRequest;
 
-function showCtxMenu(request, x, y){ 
+function showCtxMenu(request, x, y) { 
     ctxRequest = request;
     popUpNew = 1; // mark that we were clicking once
-    var ctxMenu = document.getElementById('ctxMenu'); 
-    if( ctxMenu ){
 
+    ctxX = x;
+    ctxY = y;
+
+    var ctxMenu = document.getElementById('ctxMenu'); 
+    var logArea = document.getElementById('logArea'); 
+    if( logArea && ctxMenu ) {
+        // This makes sure CTX menu is dimissing
+        document.onclick = check; 
+        logArea.onmousemove = onmouseleave; 
+        logArea.onscroll = check; 
 
         if( ( 'iframe' == request.id )
         ||  ( 'src'    == request.id )
@@ -40,17 +77,16 @@ function showCtxMenu(request, x, y){
             var domain = site.replace(/^www./, ''); // need to find a better way for this!!!
 
             ctxMenu.innerHTML =
-                '<div class="menu" onclick="clickCtx(this)">ACE Insight</div>'+
-                '<div class="menu"><a target="_blank" href="http://aceinsight.websense.com/Results.aspx?url='+url+'">ACE Insight</a></div>'+
-                '<div class="menu"><a target="_blank" href="http://www.mywot.com/en/scorecard/'+site+'">myWOT</a></div>'+
-                '<div class="menu"><a target="_blank" href="http://www.robtex.com/dns/'+site+'.html#result">robtex</a></div>'+
-                '<div class="menu"><a target="_blank" href="http://www.robtex.com/dns/'+domain+'.html#whois">whois</a></div>'+
+                '<div class="menu" onclick="openURL(\'http://aceinsight.websense.com/Results.aspx?url='+url+'\')">ACE Insight</div>'+
+                '<div class="menu" onclick="openURL(\'http://www.mywot.com/en/scorecard/'+site+'\')">myWOT</div>'+
+                '<div class="menu" onclick="openURL(\'http://www.robtex.com/dns/'+site+'.html#result\')">robtex</div>'+
+                '<div class="menu" onclick="openURL(\'http://whois.domaintools.com/'+domain+'\')">whois</div>'+
             '';
 
             // global context menu settings
-            ctxMenu.style.display='block';
-            ctxMenu.style.top=y;
-            ctxMenu.style.left=x;
+            ctxMenu.style.display = 'block';
+            ctxMenu.style.top = y;
+            ctxMenu.style.left = x;
         }
         else if ( 'script' == request.id ) {
             var url = request.innerHTML.replace(/^src=/, ''); // remove 'src='
@@ -65,12 +101,12 @@ function showCtxMenu(request, x, y){
             '';
 
             // global context menu settings
-            ctxMenu.style.display='block';
-            ctxMenu.style.top=y;
-            ctxMenu.style.left=x;
+            ctxMenu.style.display = 'block';
+            ctxMenu.style.top = y;
+            ctxMenu.style.left = x;
         }
         else {
-            ctxMenu.style.display='none';
+            ctxMenu.style.display = 'none';
         }
 
     } 
